9e13d11168522a3d742ef8972975b2613ab205e0
unknown <tboysunova@example.com>
2024-11-27 15:59:48 +0500

    monster high

--- 00017914/home/side_menu.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    const sideMenu = document.getElementById('side-menu');
-    const overlay = document.getElementById('menu-overlay');
-  
-    // Toggle the active class
-    sideMenu.classList.toggle('active');
-    overlay.classList.toggle('active');
-  }
-  
